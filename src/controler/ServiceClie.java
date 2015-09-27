16ff345817fc58ca36e5e55040f76bb82fc643df
marianaruiz <mariana@localhost>
2015-09-27 02:50:00 -0500

    Varios Archivo Null

--- src/controler/ServiceClie.java
+++ src/controler/ServiceClie.java
@@ -11,6 +11,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -28,8 +29,8 @@ public class ServiceClie extends Thread{
     private PrintWriter salidaText;
     private BufferedReader entradaText;
     private Socket socket;
-    private FileOutputStream salidaFile;
-    private FileInputStream entradaFile;
+    private ObjectOutputStream salidaFile;
+    private ObjectOutputStream entradaFile;
     private static final int PUERTO = 5000;
     
     public ServiceClie(String ip)
@@ -40,6 +41,7 @@ public class ServiceClie extends Thread{
                 this.socket = new Socket(ip,PUERTO);
                 this.salidaText = new PrintWriter(socket.getOutputStream(),true);
                 this.entradaText = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+              //  this.salidaFile= new ObjectOutputStream(socket.getOutputStream());
                 VentanaCliente.jbConnect.setText("Cerrar Sesión");
 //                System.out.println("Todo funcionando !!!!");
             }catch(IOException e){
@@ -59,6 +61,7 @@ public class ServiceClie extends Thread{
                 mensaje= this.entradaText.readLine();
                 System.out.println("Entro "+mensaje);
                 VentanaCliente.txtAreaText.setText(VentanaCliente.txtAreaText.getText() + "\n" +mensaje);
+                
             }
         } catch (IOException ex) {
                 JOptionPane.showMessageDialog(null, "No se ha podido establecer conexion ciere el programa", "Error de conexion", JOptionPane.ERROR_MESSAGE);
@@ -72,6 +75,64 @@ public class ServiceClie extends Thread{
             this.salidaText = new PrintWriter(socket.getOutputStream(),true);
             System.out.println("Se va a enviar "+ mensaje);
             salidaText.println(mensaje);
+        } catch (IOException ex) {
+            Logger.getLogger(ServiceClie.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void enviarArchivo(String fichero){
+       try
+        {
+            boolean enviadoUltimo=false;
+            // Se abre el fichero.
+            FileInputStream fis = new FileInputStream(fichero);
+            System.out.println(fichero);
+            
+            // Se instancia y rellena un mensaje de envio de fichero
+            MensajeTomaFichero mensaje = new MensajeTomaFichero();
+            mensaje.nombreFichero = fichero;
+            // Se leen los primeros bytes del fichero en un campo del mensaje
+            int leidos = fis.read(mensaje.contenidoFichero);
+            
+            // Bucle mientras se vayan leyendo datos del fichero
+            while (leidos > -1)
+            {
+                
+                // Se rellena el n�mero de bytes leidos
+                mensaje.bytesValidos = leidos;
+                
+                // Si no se han leido el m�ximo de bytes, es porque el fichero
+                // se ha acabado y este es el �ltimo mensaje
+                if (leidos < MensajeTomaFichero.LONGITUD_MAXIMA)
+                {
+                    mensaje.ultimoMensaje = true;
+                    enviadoUltimo=true;
+                }
+                else
+                    mensaje.ultimoMensaje = false;
+                
+                // Se env�a por el socket
+                salidaFile.writeObject(mensaje);
+                
+                // Si es el �ltimo mensaje, salimos del bucle.
+                if (mensaje.ultimoMensaje)
+                    break;
+                
+                // Se crea un nuevo mensaje
+                mensaje = new MensajeTomaFichero();
+                mensaje.nombreFichero = fichero;
+                
+                // y se leen sus bytes.
+                leidos = fis.read(mensaje.contenidoFichero);
+            }
+            
+            if (enviadoUltimo==false)
+            {
+                mensaje.ultimoMensaje=true;
+                mensaje.bytesValidos=0;
+                salidaFile.writeObject(mensaje);
+            }
+            // Se cierra el ObjectOutputStream
+            salidaFile.close();
         } catch (IOException ex) {
             Logger.getLogger(ServiceClie.class.getName()).log(Level.SEVERE, null, ex);
         }
